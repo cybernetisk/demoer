a3a623f5eb089f69321df633b283b0cc97bfbb08
Sigurd Ljødal <sigurd@ljodal.no>
2012-04-20 18:34:21 +0200

    Railsdemo-mappe

---
+++ railsdemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsdemo::Application.config.secret_token = 'e8f63a2dd4b5d6e4a4a1286090d9edfe61893fc2c2edc244c1400e4c1f074f36b19e893cbe513d738d2e2e88d0117c85d1aed35620c8450e687e5ffdc36f49c0'
