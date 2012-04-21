24902c1e4973322c25e22bd05601607e45ab141d
Sigurd Ljødal <sigurd@ljodal.no>
2012-04-22 00:35:26 +0200

    Fix some errors in the models

--- railsdemo/app/models/shift.rb
+++ railsdemo/app/models/shift.rb
@@ -2,5 +2,4 @@ class Shift < ActiveRecord::Base
   attr_accessible :comment, :end, :leasing, :start, :training
   belongs_to :user, dependent: :nullify
   belongs_to :shift_type, dependent: :nullify
-  belongs_to :certification, dependent: :destroy
 end
--- railsdemo/app/models/user.rb
+++ railsdemo/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :last_login, :username
   has_many :shifts, dependent: :nullify, inverse_of: :user
+  has_many :certifications, dependent: :destroy, inverse_of: :user
 end
