0e5720fa104cd0d672b2420ea6d0ba521a66a8ec
Øyvind Bakkeli <olivaq@gmail.com>
2012-04-21 22:43:08 +0200

    La til litt kommentarer

--- cakephp/app/Controller/ShiftsController.php
+++ cakephp/app/Controller/ShiftsController.php
@@ -7,19 +7,30 @@ App::uses('AppController', 'Controller');
 
 class ShiftsController extends AppController {
 
-/**
- * Scaffold
- *
- * @var mixed
- */
+	/**
+	 * Scaffold
+	 *
+	 * @var mixed
+	 */
 	public $scaffold;
+	// scaffold er en kul sak, som genererer mesteparten av sidene i prosjektet akkurat nå.
+	// alt under user, certification og shift_type autogenereres pga. at denne variabelen
+	// er satt.
 	
+	// denne funksjonen håndterer /shifts/add urlen
 	public function add()
 	{
+		// cakephp er veldig enkelt og fint for enkle ting som håndtering av 
+		// forms uten noe sær funksjonalitet og lagring av disse, det kan gjøres som under
+		 
+		// er dette en POST request?
 		if($this->request->isPost())
 		{
+			// i såfall, prøv å lagre. Save validerer også dataen opp mot modellen,
+			// og om noe er feil så returnerer den false
 			if($this->Shift->save($this->request->data))
 			{
+				// alt er ok, så send brukeren til "view" siden for det nye skiftet
 				$this->redirect('/shifts/view/' . $this->Shift->id);
 			}
 		}
@@ -28,10 +39,14 @@ class ShiftsController extends AppController {
 		$this->set('shiftTypes', $this->Shift->ShiftType->find('list'));
 	}
 	
+	// denne funksjonen erstatter /shifts siden (index er "default")
 	public function index()
 	{
+		// kaller på getAvailableShifts metoden i Shift modellen, som returnerer ledige skift
 		$shifts = $this->Shift->getAvailableShifts();
 		
+		// lagerer variabelen så man får tak i den fra viewet
+		// compact('shift') == array('shift'=>$shift) (en dictionary hvor shift peker på $shift - resultatet vi hentet)
 		$this->set(compact('shifts'));
 	}
 }
--- cakephp/app/Model/Shift.php
+++ cakephp/app/Model/Shift.php
@@ -8,20 +8,18 @@ App::uses('AppModel', 'Model');
  * @property Verified $Verified
  */
 class Shift extends AppModel {
-/**
- * Display field
- *
- * @var string
- */
+	/**
+	 * Display field
+	 *
+	 * @var string
+	 */
 	public $displayField = 'shift_type_id';
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-/**
- * belongsTo associations
- *
- * @var array
- */
+	/**
+	 * belongsTo associations
+	 *
+	 * @var array
+	 */
 	public $belongsTo = array(
 		'ShiftType',
 		'User',
@@ -31,14 +29,22 @@ class Shift extends AppModel {
 	
 	public $validate = array(
 		'start' => array('rule'=>'notempty', 'message'=>'You need a start for the shift'),
+		// php er litt snodig når det gjelder referanser til funksjoner innimellom, så her sender vi bare med
+		// navnet til funksjonen som en string i rule (se funksjonen rett under her)
 		'stop' => array('rule'=>'checkStopDate','message'=>'Stop should be after start')
 			);
 	
+	// Denne funksjonen sjekker om stop tidspunktet er etter start tidspunktet, og brukes til validering av dataen
 	function checkStopDate($stop)
 	{
+		// strtotime er en fin funksjon som takler nesten alt av dato/tidstrenger og returnerer
+		// unix timestamp som vi kan sammenlikne enklere.
 		return strtotime($this->data['Shift']['stop']) > strtotime($this->data['Shift']['start']);
 	}
 	
+	// find('all', ...) returnerer masse data, den henter relasjoner rekusivt og mye fint. Innimellom blir det
+	// for mye av det gode, cakephp kan laste for mye data og bli tregt om det er store applikasjoner, men er
+	// relativt lett å jobbe seg rundt det.
 	function getAvailableShifts($shift_type = null)
 	{
 		return $this->find('all', array('conditions'=>array('Shift.user_id'=>null)));
