e06c3e1721be91dcf838d58247a7a8c6379456c4
Henrik Steen <henrist@henrist.net>
2013-09-06 22:00:43 +0200

    Basic shifts and shift-types linked with volunteer.

--- django/shifty_shifts/models.py
+++ django/shifty_shifts/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Shift(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    shift_type = models.ForeignKey("ShiftType", null=False)
+    volunteer = models.ForeignKey(User, null=True, blank=True)
     start = models.DateTimeField()
     stop = models.DateTimeField()
-
-    def __str__(self):
+    
+    def __unicode__(self):
         return self.title
+
+    def getType(self):
+        return self.shift_type
+    getType.short_description = "Shift-type"
+
+
+class ShiftType(models.Model):
+    def __unicode__(self):
+        return self.title
+
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
